ba2169d8d73a4e837ef482afd2d7ffa931fa33ff
Usha Nemani <venus5063@gmail.com>
2024-05-16 07:25:18 +0530

    week 1 and week 2 updated

--- week1_intro/docker_intro/pipeline.py
+++ week1_intro/docker_intro/pipeline.py
@@ -5,3 +5,6 @@ print(sys.argv)
 
 
 print("pandas is imported successfully")
+
+
+"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-01.csv.gz" "https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv"
--- week1_intro/terraform_gcp/main.tf
+++ week1_intro/terraform_gcp/main.tf
@@ -12,29 +12,11 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_storage_bucket" "terraform-demo" {
-  name                        = "${local.data_lake_bucket}_${var.project}"
-  # Concatenating DL bucket & Project name for unique naming
-  location                    = var.region
-  force_destroy               = true
-  # Optional, but recommended settings:
-  storage_class               = var.storage_class
-  uniform_bucket_level_access = true
-
-  lifecycle_rule {
-    condition {
-      age = 85
-    }
-    action {
-      type = "Delete"
-    }
-  }
-}
 
 
 # DWH
 # Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
-resource "google_bigquery_dataset" "demo-dataset" {
+resource "google_bigquery_dataset" "ny-taxi-data" {
   dataset_id = var.BQ_DATASET
   project    = var.project
   location   = var.region
