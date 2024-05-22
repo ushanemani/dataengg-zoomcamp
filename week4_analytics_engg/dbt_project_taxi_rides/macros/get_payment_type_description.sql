76184f5da053dc0d4b122050e673d994d29a2bb6
Usha Nemani <venus5063@gmail.com>
2024-05-22 16:52:49 +0000

    final files

--- week4_analytics_engg/dbt_project_taxi_rides/macros/get_payment_type_description.sql
+++ week4_analytics_engg/dbt_project_taxi_rides/macros/get_payment_type_description.sql
@@ -1,35 +1,23 @@
-<<<<<<< HEAD
- {#
-    This macro returns the description of the payment_type 
-#}
-
-{% macro get_payment_type_description(payment_type) -%}
-
-    case {{ payment_type }}
-        when 1 then 'Credit card'
-        when 2 then 'Cash'
-        when 3 then 'No charge'
-        when 4 then 'Dispute'
-        when 5 then 'Unknown'
-        when 6 then 'Voided trip'
-    end
-
-=======
 {#
     This macro returns the description of the payment_type 
 #}
-
 {% macro get_payment_type_description(payment_type) -%}
 
-    case {{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }}  
-        when 1 then 'Credit card'
-        when 2 then 'Cash'
-        when 3 then 'No charge'
-        when 4 then 'Dispute'
-        when 5 then 'Unknown'
-        when 6 then 'Voided trip'
+    case
+        {{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }}
+        when 1
+        then 'Credit card'
+        when 2
+        then 'Cash'
+        when 3
+        then 'No charge'
+        when 4
+        then 'Dispute'
+        when 5
+        then 'Unknown'
+        when 6
+        then 'Voided trip'
         else 'EMPTY'
     end
 
->>>>>>> cd5185c07cb9957ab4f17bd15bb10e720dd32790
 {%- endmacro %}
